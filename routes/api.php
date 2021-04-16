c06b2d5dac4ca451640819f037ebb84f1d154d95
Gilson96 <grafael99@gmail.com>
2021-04-16 11:45:19 +0100

    Added all end-points to Person's routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,37 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Persons;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+
+// all of the routes are in the /persons end-point
+Route::group([
+    "prefix" => "persons",
+   // "middleware" => ["auth:api"],
+], function () {
+
+    // GET /persons: show all persons
+    Route::get("", [Persons::class, "index"]);//->middleware('auth:api');
+
+    // POST /persons: create a new person
+    Route::post("", [Persons::class, "store"]);
+
+    // all these routes also have an person ID in the
+    // end-point, e.g. /persons/8
+    Route::group(["prefix" => "{persons}"], function () {
+
+        // GET /persons/8: show the client
+        Route::get("", [Persons::class, "show"]);
+        // PUT /persons/8: update the client
+        Route::put("", [Persons::class, "update"]);
+        // DELETE /persons/8: delete the client
+        Route::delete("", [Persons::class, "destroy"]);
+    });
 });
